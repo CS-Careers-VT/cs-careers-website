d497fe5f7bb74857ba8c71e9e9f52f5eb32be8de
Blake M <blake@marterella.com>
2025-02-07 11:30:11 -0500

    Update TailwindCSS styles
    Configure the basic style guides for tailwindcss:
    - Added Google Outfit font to `html` and set as
    default for Tailwindcss
    - Installed the aniamted tailwindcss plugin
    - Add cs careers colors:
        - `csc-maroon`: maroon
        - `csc-orange`: orange
        - `csc-yellow`: yellow
    
    In the futuer, I will add different gradients of the
    cs careers colors so that we can have dynamic
    change without having to hard code any color
    values.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Outfit', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        'csc-maroon': {
+          default: '#800020'
+        },
+        'csc-organge': {
+          default: '#D6995D'
+        },
+        'csc-yellow': {
+          default: '#ECD68F'
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated'),
+    (process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+  ],
 }
 
