e8dffbb49ae5c0cc32010910691e561639786176
Bxateng <aaronbo2003@gmail.com>
2025-05-24 17:52:57 -0400

    Added exec member cards

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,5 +1,20 @@
 import FirstYearPhoto from "@assets/pics/first_year_experience.jpg"
 import MicrosoftPhoto from "@assets/pics/msft_event.png"
+import Alexa from "@assets/pics/Alexa.jpeg"
+import Amy from "@assets/pics/Amy.jpeg"
+import Harita from "@assets/pics/Harita.jpeg"
+import Lucas from "@assets/pics/Lucas.jpeg"
+import Nina from "@assets/pics/Nina.jpeg"
+import Reet from "@assets/pics/Reet.jpeg"
+import Aaron from "@assets/pics/Aaron.jpeg"
+import Gabe from "@assets/pics/Gabe.jpeg"
+import Gianfranco from "@assets/pics/Gianfranco.jpg"
+import Alex from "@assets/pics/Alex.jpeg"
+import Lauren from "@assets/pics/Lauren.jpeg"
+import Sam from "@assets/pics/Sam.jpeg"
+import Stuti from "@assets/pics/Stuti.jpeg"
+import Tarun from "@assets/pics/Tarun.jpeg"
+import ExecCard from "@components/common/ExecCard";
 
 function AboutUs() {
     return (
@@ -12,21 +27,21 @@ function AboutUs() {
             <div className="flex flex-wrap justify-center gap-6 mt-20 mb-60">
                 {/* Box 1 */}
                 <div className="w-80 h-60 relative rounded-[50px]">
-                    <div className="w-80 h-60 left-0 top-0 absolute bg-orange-400 rounded-[50px]"></div>
+                    <div className="w-80 h-60 left-0 top-0 absolute bg-[#D6995D] rounded-[50px]"></div>
                     <div className="left-[70px] top-[160px] absolute justify-start text-white text-4xl font-semibold font-['Outfit'] leading-7 tracking-widest">Members</div>
                     <div className="left-[23px] top-[67px] absolute justify-start text-white text-8xl font-semibold font-['Outfit'] leading-7 tracking-[5px]">1200+</div>
                 </div>
 
                 {/* Box 2 */}
                 <div className="w-80 h-60 relative rounded-[50px]">
-                    <div className="w-80 h-60 left-0 top-0 absolute bg-orange-400 rounded-[50px]"></div>
+                    <div className="w-80 h-60 left-0 top-0 absolute bg-[#D6995D] rounded-[50px]"></div>
                     <div className="left-[100px] top-[160px] absolute justify-start text-white text-4xl font-semibold font-['Outfit'] leading-7 tracking-widest">Events</div>
                     <div className="w-56 h-20 left-[88px] top-[67px] absolute justify-start text-white text-8xl font-semibold font-['Outfit'] leading-7 tracking-[5px]">30+</div>
                 </div>
 
                 {/* Box 3 */}
                 <div className="w-80 h-60 relative rounded-[50px]">
-                    <div className="w-80 h-60 left-0 top-0 absolute bg-orange-400 rounded-[50px]"></div>
+                    <div className="w-80 h-60 left-0 top-0 absolute bg-[#D6995D] rounded-[50px]"></div>
                     <div className="w-60 left-[36px] top-[141px] absolute justify-start text-white text-4xl font-semibold font-['Outfit'] leading-10 tracking-widest">Largest CS Organization</div>
                     <div className="left-[102px] top-[67px] absolute justify-start text-white text-8xl font-semibold font-['Outfit'] leading-7 tracking-[5px]">#1</div>
                 </div>
@@ -35,7 +50,7 @@ function AboutUs() {
             {/* Serving the Community */}
             <div className="w-full min-h-[850px] bg-csc-organge-bg px-4 py-20 flex flex-col items-center">
                 <h2 className="text-white text-5xl md:text-6xl font-semibold font-['Outfit'] text-center">
-                    Serving the Community
+                    Serving The Community
                 </h2>
 
                 <div className="mt-1 w-4/5 md:w-[620px] h-4 bg-csc-maroon-bg"></div>
@@ -56,6 +71,106 @@ function AboutUs() {
                         <img src={MicrosoftPhoto} alt="Microsoft Event" className="w-full max-w-md rounded-2xl shadow-lg" />
                         <img src={FirstYearPhoto} alt="First Year Experience" className="w-full max-w-md rounded-2xl shadow-lg" />
                     </div>
+                </div>
+            </div>
+
+            {/* Meet The Executive Board */}
+            <div className="w-full bg-csc-maroon-bg text-center mt-20 px-4">
+                <h2 className="text-white text-5xl md:text-6xl font-semibold font-['Outfit']">
+                    Meet The Executive Board!
+                </h2>
+            </div>
+            <div className="w-[720px] h-[11px] bg-csc-organge-bg mx-auto"></div>
+
+            {/* Board of Directors */}
+            <div className="mt-20 px-4 max-w-7xl mx-auto">
+                <h3 className="text-5xl text-white font-semibold font-serif text-center">
+                    Board of Directors
+                </h3>
+            
+                <div className="mt-12 flex flex-wrap justify-center gap-10">
+                    <ExecCard
+                    name="Lauren Ruiz-Arenas"
+                    role="President"
+                    photo={Lauren}
+                    />
+                    <ExecCard
+                    name="Gianfranco Vivanco"
+                    role="Co-Vice President"
+                    photo={Gianfranco}
+                    />
+                    <ExecCard
+                    name="Alex Brown"
+                    role="Co-Vice President"
+                    photo={Alex}
+                    />
+                    <ExecCard
+                    name="Tarun Nandamudi"
+                    role="Operations Director"
+                    photo={Tarun}
+                    />
+                    <ExecCard
+                    name="Stuti Shah"
+                    role="Finance Director"
+                    photo={Stuti}
+                    />
+                    <ExecCard
+                    name="Sam Jarvis"
+                    role="Co-Marketing Director"
+                    photo={Sam}
+                    />
+                    <ExecCard
+                    name="Alexa Thompson"
+                    role="Co-Marketing Director"
+                    photo={Alexa}
+                    />
+                    <ExecCard
+                    name="Gabriel Palomino"
+                    role="Opportunities Chair"
+                    photo={Gabe}
+                    />
+                    <ExecCard
+                    name="Aaron Boateng"
+                    role="Webmaster"
+                    photo={Aaron}
+                    />
+
+                </div>
+            </div>
+
+
+            {/* Officers */}
+            <div className="mt-12 px-4 max-w-7xl mx-auto">
+                <h3 className="text-5xl text-white font-semibold font-['Outfit'] text-center">
+                    Officers
+                </h3>
+            
+                <div className="mt-12 flex flex-wrap justify-center gap-10">
+                    <ExecCard
+                    name="Nina Yang"
+                    role="Outreach Chair"
+                    photo={Nina}
+                    />
+                    <ExecCard
+                    name="Amy Weston"
+                    role="Fundraising Chair"
+                    photo={Amy}
+                    />
+                    <ExecCard
+                    name="Reet Kaler"
+                    role="Graphic Designer"
+                    photo={Reet}
+                    />
+                    <ExecCard
+                    name="Harita Kondragunta"
+                    role="Co-Social Media Chair"
+                    photo={Harita}
+                    />
+                    <ExecCard
+                    name="Lucas Lombardi"
+                    role="Co-Social Media Chair"
+                    photo={Lucas}
+                    />
                 </div>
             </div>
 
