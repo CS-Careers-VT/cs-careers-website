537515f1a40fa0d6115ccfa1a7f992c152990e5c
Bxateng <aaronbo2003@gmail.com>
2025-05-26 22:07:05 -0400

    Added semester header

--- src/pages/NewsEvents.tsx
+++ src/pages/NewsEvents.tsx
@@ -2,8 +2,10 @@
 function NewsEvents() {
     return (
         <div>
-            <h1>NewsEvents</h1>
-            <p>Welcome to the NewsEvents page!</p>
+            <div className="w-full bg-csc-maroon-bg text-center mt-20">
+                <h2>Spring 2025</h2>
+            </div>
+            <div className="w-[270px] h-[11px] bg-csc-organge-bg mx-auto mt-2"></div>
         </div>
     )
 }
